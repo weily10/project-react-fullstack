3d09617a9cea15b00fb1b3d7a72eea5c25007f2b
weily huang <w3ily10@gmail.com>
2024-01-22 22:45:50 +0800

    connect db

--- api/index.js
+++ api/index.js
@@ -1,4 +1,14 @@
 import express from "express";
+import mongoose from "mongoose"
+import dotenv from "dotenv"
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(()=>{
+  console.log('success');
+}).catch((err)=>{
+  console.log('error');
+})
 
 const app = express();
 

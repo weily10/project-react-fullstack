dfecfd2d1869b85cbe7499e2d5456601d25c38df
weily huang <w3ily10@gmail.com>
2024-04-10 22:31:33 +0800

    sign up functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,32 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate()
+  
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e) => {
+    
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+      if (data.success === false) {
+        setError(data.message);
+        setLoading(false);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate('/sign-in')
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError(error.message);
+    }
+
+  };
   return (
-    <div className="flex justify-center items-center flex-col h-[100dvh] gap-3">
-       <h3 className="text-gray-500">Welcome! Pls SignUp</h3>
-      <div className="border border-gray-200 p-6">
-       
-        <div className="flex justify-start  flex-col">
-          <label htmlFor="input1" className="text-start text-neutral-500">
-            name
-          </label>
-          <input
-            type="text"
-            id="input1"
-            className=" w-40 border border-gray-300 h-9"
-          />
-        </div>
-        <div className="flex justify-start  flex-col mt-3">
-          <label htmlFor="input1" className="text-start text-neutral-500">
-            password
-          </label>
-          <input
-            type="text"
-            id="input1"
-            className=" w-40 border border-gray-300 h-9"
-          />
+    <form onSubmit={handleSubmit}>
+      <div className="flex justify-center items-center flex-col h-[100dvh] gap-3">
+        <h1 className="text-gray-500 text-2xl">Sign Up </h1>
+        <div className="border border-gray-200 p-9 w-1/2 flex flex-col gap-3">
+          <div className="flex justify-start  flex-col">
+            <label
+              htmlFor="username"
+              className="text-start text-neutral-500 text-sm"
+            >
+              username
+            </label>
+            <input
+              placeholder="username"
+              type="text"
+              id="username"
+              className="w-full border border-gray-300 h-12 p-3 rounded mt-1"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex justify-start  flex-col">
+            <label
+              htmlFor="email"
+              className="text-start text-neutral-500 text-sm"
+            >
+              email
+            </label>
+            <input
+              placeholder="email"
+              type="text"
+              id="email"
+              className="w-full border border-gray-300 h-12 p-3 rounded mt-1"
+              onChange={handleChange}
+            />
+          </div>
+          <div className="flex justify-start  flex-col ">
+            <label
+              htmlFor="password"
+              className="text-start text-neutral-500 text-sm"
+            >
+              password
+            </label>
+            <input
+              placeholder="must have at leaast 6 characters"
+              type="text"
+              id="password"
+              className="w-full border border-gray-300 h-12 p-3 rounded mt-1"
+              onChange={handleChange}
+            />
+          </div>
+          {error && <p className="text-red-500 mt-5">{error}</p>}
+          <div className="mt-3">
+            <button
+              disabled={loading}
+              className="bg-blue-700 w-full rounded p-3 text-white hover:bg-blue-500"
+            >
+              {loading ? "loading" : "SIGN UP"}
+            </button>
+          </div>
+          <div>
+            <button className="bg-red-600 w-full rounded p-3 text-white hover:bg-red-500">
+              GOOGLE
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    </form>
   );
 }
